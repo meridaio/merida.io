c38166f04ae36aab1bee1bcada519233ab8e41c5
meridaio <meridaio@users.noreply.github.com>
2021-04-20 01:34:24 +0000

    deploy: 8b1b9de4113410370ace3128cc19844ac3b942c4

--- js/bg-picker.js
+++ js/bg-picker.js
@@ -2,7 +2,7 @@
  * Picks one of 4 backgrounds for the site based on the current time of year in the northern hemisphere
  */
 (() => {
-  document.documentElement.style.setProperty('--background-image', "url('images/" + (() => {
+  document.documentElement.style.setProperty('--homepage-background', "url('images/" + (() => {
     const month = new Date().getMonth()
     if (month == 11 || month < 2) return "winter-bw.jpg"
     if (month < 5) return "spring-bw.jpg"
